feace3340bfca97d804e9d1ebd412baec09f3090
priyamkanojia <kanojiapriyam1499@gmail.com>
2021-06-09 22:21:10 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(title: Center(child: Text('Ask Me Anything')), backgroundColor: Colors.black45),
+          backgroundColor: Colors.white,
+          body: MyApp(),
+        ),
+      ),
+    );
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int decision =1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+          onPressed:(){
+            setState(() {
+              decision=Random().nextInt(5)+1;
+            });
+            print(decision);
+          },
+          child: Image.asset('images/ball$decision.png'),
+      ),
+    );
+  }
+}
